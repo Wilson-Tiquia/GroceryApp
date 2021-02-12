82c1e248f46f523a6aa1015be7724d925987f59f
wilson <jwtiquia@gmail.com>
2021-02-12 22:46:49 +0800

    Getting user input sign up pa lang

--- app/src/main/java/com/example/groceryapp/SignUp.java
+++ app/src/main/java/com/example/groceryapp/SignUp.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
 
@@ -14,8 +18,25 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
     }
 
-    public void goToEmailVerification(View v){
-        Intent intent = new Intent(SignUp.this, EmailVerification.class);
-        startActivity(intent);
+    public void handleText (View v){
+
+        // pag nakuha mo na id nya pwede mo makuha value
+        EditText [] inputArray = new EditText[]{
+                 findViewById(R.id.usernameInput),
+                 findViewById(R.id.emailInput),
+                findViewById(R.id.passwordInput)
+        };
+        String [] credentialInputString = new String[inputArray.length];
+        for(int i =0; i<inputArray.length;i++){
+            credentialInputString[i] = inputArray[i].getText().toString();
+            Log.d("info", credentialInputString[i]);
+
+        }
     }
+    public void goToEmailVerification(View v){
+        //Intent intent = new Intent(SignUp.this, EmailVerification.class);
+        //startActivity(intent);
+
+    }
+
 }
