086da18846ee9cdba2194a571614322c8b6ad9c5
wilson <jwtiquia@gmail.com>
2021-02-12 15:19:29 +0800

    Splash Screen, Log In, Sign Up, And Verification Page (di pa gumagana)

--- app/src/main/java/com/example/groceryapp/MainActivity.java
+++ app/src/main/java/com/example/groceryapp/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.groceryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int splashTime = 1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, EmailVerification.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, splashTime);
     }
 }
