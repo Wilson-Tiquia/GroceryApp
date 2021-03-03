e2ead597b7225b962f5590f461367a6f37a180a8
wilson <jwtiquia@gmail.com>
2021-03-04 03:26:21 +0800

    Chosen Item page

--- app/src/main/java/com/example/groceryapp/GroceryItem.java
+++ app/src/main/java/com/example/groceryapp/GroceryItem.java
@@ -6,23 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class LandingPage extends AppCompatActivity {
+public class GroceryItem extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing_page);
+        setContentView(R.layout.activity_grocery_item);
     }
-
-
     public void goToSettings(View v){
         Intent intent = new Intent(this, Settings.class);
         startActivity(intent);
     }
-    public void goToChosenItem(View v){
-        Intent intent = new Intent(this, GroceryItem.class);
+    public void goToLandingPage(View v){
+        Intent intent = new Intent(this, LandingPage.class);
         startActivity(intent);
     }
 }
-
-
